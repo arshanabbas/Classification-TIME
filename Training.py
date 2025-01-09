f848cc6ae3e24f9d8d73de545d27fea52f09ac05
Arshan Abbas <arshan.abbas@student.uni-siegen.de>
2025-01-09 14:06:33 +0100

    Update Training.py

--- Training.py
+++ Training.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
 
-results = model.train(data="mnist160", epochs=1, imgsz=64)
+results = model.train(data="C:/Users/arab/Documents/GitHub/ClassificationData/Output", epochs=100, imgsz=1024)
